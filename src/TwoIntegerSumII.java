f37de3138fa289c730f3f9a48ff004b240774126
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2025-01-20 16:13:27 -0300

    feat: solution with two pointers two integer sum II

--- src/TwoIntegerSumII.java
+++ src/TwoIntegerSumII.java
@@ -10,27 +10,20 @@ public class TwoIntegerSumII {
     }
 
     public int[] twoSum(int[] numbers, int target) {
-        int[] output = new int[2];
+       int left = 0;
+       int right = numbers.length - 1;
 
+       while (left < right) {
+           int sum = numbers[left] + numbers[right];
+           if (sum > target) {
+                right -= 1;
+           } else if (sum < target) {
+                left += 1;
+           } else {
+                return new int[] {left+1, right+1};
+           }
+       }
 
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = 0; j < numbers.length; j++) {
-
-                if (i == j) {
-                    continue;
-                }
-
-
-                if (numbers[i] + numbers[j] == target) {
-                    output[0] = i + 1;
-                    output[1] = j + 1;
-                    return output;
-                }
-            }
-        }
-
-
-        return output;
+       return new int[] {left+1, right+1};
     }
-
 }
